567d816d9b0675f19f3f18a669e1625d778deee0
Allie Soliz <solizale@gmail.com>
2023-06-17 12:52:50 -0700

    modifies container component:StudentList to include list of component:Student

--- src/components/StudentList.js
+++ src/components/StudentList.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import Student from './Student';
 import './StudentList.css';
 
 const StudentList = () => {
@@ -6,9 +7,12 @@ const StudentList = () => {
 		<section>
 			<h2 className="student-list__heading">Student List</h2>
 			<ul className="student-list">
-				<li>Student A</li>
-				<li>Student B</li>
-				<li>Student C</li>
+				<li>
+					<Student></Student>
+				</li>
+				<li>
+					<Student></Student>
+				</li>
 			</ul>
 		</section>
 	);
