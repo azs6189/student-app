277fc05e11d9a698c546e4067af5047cd13ce82e
Allie Soliz <solizale@gmail.com>
2023-06-17 18:16:07 -0700

    creates list item holding <Student/> JSX elements and embeds it in the returned JSX element

--- src/components/StudentList.js
+++ src/components/StudentList.js
@@ -3,22 +3,23 @@ import Student from './Student';
 import './StudentList.css';
 
 const StudentList = () => {
+	const studentComponents = [
+		<li>
+			{/* Shorthand for displaying components 
+                    previously: <Student></Student> */}
+			<Student name="Ada" email="ada@dev.org" />
+		</li>,
+		<li>
+			<Student name="Soo-ah" email="sooh@dev.org" />
+		</li>,
+		<li>
+			<Student name="Chrissy" email="chrissy@dev.org" />
+		</li>,
+	];
 	return (
 		<section>
 			<h2 className="student-list__heading">Student List</h2>
-			<ul className="student-list">
-				<li>
-					{/* Shorthand for displaying components 
-                    previously: <Student></Student> */}
-					<Student name="Ada" email="ada@dev.org" />
-				</li>
-				<li>
-					<Student name="Soo-ah" email="sooh@dev.org" />
-				</li>
-				<li>
-					<Student name="Chrissy" email="chrissy@dev.org" />
-				</li>
-			</ul>
+			<ul className="student-list">{studentComponents}</ul>
 		</section>
 	);
 };
