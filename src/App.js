d326c1b01e691eedad0f51a18aa1fbf2771bed48
Allie Soliz <solizale@gmail.com>
2023-06-19 01:20:21 -0400

    creates prop:memberCount for value related to the number of students in studentData

--- src/App.js
+++ src/App.js
@@ -2,11 +2,26 @@ import StudentList from './components/StudentList';
 import ClassInfo from './components/ClassInfo';
 
 function App() {
+	const studentData = [
+		{
+			namdData: 'Ada',
+			emailData: 'ada@dev.org',
+		},
+		{
+			namdData: 'Soo-ah',
+			emailData: 'sooah@dev.org',
+		},
+		{
+			namdData: 'Chrissy',
+			emailData: 'chrissy@dev.org',
+		},
+	];
+
 	return (
 		<main>
 			<h1>Attendance</h1>
-			<ClassInfo></ClassInfo>
-			<StudentList></StudentList>
+			<ClassInfo memberCount={studentData.length}></ClassInfo>
+			<StudentList students={studentData}></StudentList>
 		</main>
 	);
 }
