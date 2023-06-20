99e1802e38413d9206e9f249fd113e235508d26c
Allie Soliz <solizale@gmail.com>
2023-06-20 19:11:59 -0400

    applies green/red to present/absent student

--- src/components/Student.js
+++ src/components/Student.js
@@ -1,16 +1,18 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { useState } from 'react';
+import './Student.css';
 
 const Student = (props) => {
 	const [isPresent, setIsPresent] = useState(false);
 	const togglePresence = () => {
 		setIsPresent(!isPresent);
 	};
+	const nameColor = isPresent ? 'green' : 'red';
 	return (
 		<div>
 			<ul>
-				<li>Nickname: {props.nameData}</li>
+				<li className={nameColor}>Nickname: {props.nameData}</li>
 				<li>Email: {props.emailData}</li>
 			</ul>
 			<button onClick={togglePresence}>
