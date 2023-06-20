18eefb99c571730bdda032b1ca9a6685ea21a8fb
Allie Soliz <solizale@gmail.com>
2023-06-20 18:26:29 -0400

    uses state

--- src/components/Post.js
+++ src/components/Post.js
@@ -1,6 +1,9 @@
 import React from 'react';
+import { useState } from 'react';
 
 const Post = () => {
+	const [likesCount, setLikesCount] = useState(0);
+
 	const printMessage = (event) => {
 		console.log("Hello! We're in printMessage!");
 		console.log('This event object contains details about the event:', event);
@@ -8,6 +11,7 @@ const Post = () => {
 
 	return (
 		<section>
+			<p>What is the number of likes we should display? {likesCount}</p>
 			{/* <button onClick={printMessage}>Like</button> */}
 
 			{/* anonymous function */}
