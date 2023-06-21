ba03dcf11a374ca6eac73d41ccf4282c01b6e11b
Allie Soliz <solizale@gmail.com>
2023-06-21 18:20:54 -0400

    removes comments

--- src/components/Student.js
+++ src/components/Student.js
@@ -4,18 +4,16 @@ import { useState } from 'react';
 import './Student.css';
 
 const Student = (props) => {
-	const [isPresent, setIsPresent] = useState(false);
-	const togglePresence = () => {
-		setIsPresent(!isPresent);
-	};
-	const nameColor = isPresent ? 'green' : 'red';
+
+	const nameColor = props.isPresent ? 'green' : 'red';
+
 	return (
 		<div>
 			<ul>
 				<li className={nameColor}>Nickname: {props.nameData}</li>
 				<li>Email: {props.emailData}</li>
 			</ul>
-			<button onClick={togglePresence}>
+			<button onClick={/* togglePresence */}>
 				Toggle if {props.name} is present
 			</button>
 		</div>
@@ -28,6 +26,7 @@ Student.propTypes = {
 	name: PropTypes.string.isRequired,
 	email: PropTypes.string.isRequired,
 	isPresent: PropTypes.bool,
+    onUpdate: PropTypes.func.isRequired
 };
 
 export default Student;
